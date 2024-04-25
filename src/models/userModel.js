dd9dbd283b61926c9dc1387d794354453af25d95
SarahOtmane <sarahotmane02@gmail.com>
2024-04-25 17:01:11 +0200

    "Add bcrypt for password hashing and sync User model in bdd"

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: "db",
@@ -42,6 +43,29 @@ const User = sequelize.define('User', {
     timestamps: true,
     underscored: true
 });
+
+// Hash avant de sauvegarder en base de données
+User.addHook('beforeSave', async (user) => {
+    try {
+      // Valeur par défaut de l'algorithme de hashage : 10
+        const algo = await bcrypt.genSalt(10);
+        const hashPw = await bcrypt.hash(user.password, algo);
+        user.password = hashPw;
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+// Synchronisation du modèle avec la base de données
+(async () => {
+    try {
+        //ne pas forcer a supp et recréer la table
+        await User.sync({ force: false });
+        console.log("Modèle User synchronisé avec la base de données.");
+    } catch (error) {
+        console.error("Erreur lors de la synchronisation du modèle User:", error);
+    }
+})();
 
 
 module.exports = User;
