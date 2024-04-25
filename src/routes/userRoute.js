27702772dd3294906883f9aba37be095ad76fcd1
SarahOtmane <sarahotmane02@gmail.com>
2024-04-25 22:32:46 +0200

    add verifyToken to the userRoutes

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
 
 router
     .route('/register')
@@ -12,6 +13,7 @@ router
 
 router
     .route('/')
+    .all(jwtMiddleware.verifyToken)
     .get(userController.getAUser)
     .put(userController.putAUser)
     .delete(userController.deleteAUser)
