1c430551171329432fc59572d17446659280f07c
SarahOtmane <sarahotmane02@gmail.com>
2024-04-25 15:59:35 +0200

    "Add express server with specified port and listening for requests"
    This diff adds the following changes to the code:
    
    * Defines a constant `port` set to the value 3003.
    * Adds code to

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 
 const app = express();
+const port = 3003;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
 
+// Démarrage du serveur
+app.listen(port, () => {
+    console.log(`L'application écoute sur le port ${port}`);
+});
 
 
