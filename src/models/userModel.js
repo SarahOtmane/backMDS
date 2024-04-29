aa3aa35ecee2581b870d838e57ca40693084371f
SarahOtmane <sarahotmane02@gmail.com>
2024-04-29 20:34:45 +0200

    modif user Model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -8,23 +8,11 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 
 
 const User = sequelize.define('User', {
-    id_user: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    firstname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    lastname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        primaryKey: true,
     },
     role:{
         type: DataTypes.STRING,
@@ -36,6 +24,10 @@ const User = sequelize.define('User', {
     },
     password: {
         type: DataTypes.STRING,
+        allowNull: false
+    },
+    id_detail: {
+        type: DataTypes.INTEGER,
         allowNull: false
     },
 }, {
