2c424d704384bf86ebd9784e8a3dd5793efebce5
SarahOtmane <sarahotmane02@gmail.com>
2024-05-01 22:20:24 +0200

    model pour la table jobs : correspond a la spécialisation de l artisan

---
+++ src/models/jobModel.js
@@ -0,0 +1,38 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: "db",
+    dialect: "mysql"
+});
+
+
+const Job = sequelize.define('Job', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+}, {
+    tableName: 'jobs',
+    timestamps: true,
+    underscored: true
+});
+
+// Synchronisation du modèle avec la base de données
+(async () => {
+    try {
+        //ne pas forcer a supp et recréer la table
+        await Job.sync({ force: false });
+        console.log("Modèle Job synchronisé avec la base de données.");
+    } catch (error) {
+        console.error("Erreur lors de la synchronisation du modèle Job:", error);
+    }
+})();
+
+
+module.exports = User;
