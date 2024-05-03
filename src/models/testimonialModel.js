eb17fd74d673d56eb252c0b356b6e7b02d53f060
SarahOtmane <sarahotmane02@gmail.com>
2024-05-03 23:11:07 +0200

    l ajout de la cle etrangère id_user a la table testimonial

--- src/models/testimonialModel.js
+++ src/models/testimonialModel.js
@@ -25,6 +25,10 @@ const Testimonial = sequelize.define('Testimonial', {
         type: DataTypes.INTEGER,
         allowNull: false
     },
+    id_user: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
 }, {
     tableName: 'testimonials',
     timestamps: true,
