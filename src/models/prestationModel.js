7fb0b1c2a8097baa43a467c9b49f28c6ba7def63
SarahOtmane <sarahotmane02@gmail.com>
2024-05-03 23:12:51 +0200

    modif le nom de la table reparation => prestation

--- src/models/prestationModel.js
+++ src/models/prestationModel.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 
-const Reparation = sequelize.define('Reparation', {
+const Prestation = sequelize.define('Prestation', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
@@ -30,7 +30,7 @@ const Reparation = sequelize.define('Reparation', {
         allowNull: false
     },
 }, {
-    tableName: 'reparations',
+    tableName: 'prestations',
     timestamps: true,
     underscored: true
 });
@@ -39,12 +39,12 @@ const Reparation = sequelize.define('Reparation', {
 (async () => {
     try {
         //ne pas forcer a supp et recréer la table
-        await Reparation.sync({ force: false });
-        console.log("Modèle Reparation synchronisé avec la base de données.");
+        await Prestation.sync({ force: false });
+        console.log("Modèle Prestation synchronisé avec la base de données.");
     } catch (error) {
-        console.error("Erreur lors de la synchronisation du modèle Reparation:", error);
+        console.error("Erreur lors de la synchronisation du modèle Prestation:", error);
     }
 })();
 
 
-module.exports = Reparation;
+module.exports = Prestation;
