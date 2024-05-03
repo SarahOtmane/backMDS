a895b6427240294307b49c75fec046dbea55a1ca
SarahOtmane <sarahotmane02@gmail.com>
2024-05-03 23:22:14 +0200

    modele pour la table info (ex id : 1 name : insta content : insatLink )

---
+++ src/models/infoModel.js
@@ -0,0 +1,42 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: "db",
+    dialect: "mysql"
+});
+
+
+const Info = sequelize.define('Info', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    tableName: 'infos',
+    timestamps: true,
+    underscored: true
+});
+
+// Synchronisation du modèle avec la base de données
+(async () => {
+    try {
+        //ne pas forcer a supp et recréer la table
+        await Info.sync({ force: false });
+        console.log("Modèle Info synchronisé avec la base de données.");
+    } catch (error) {
+        console.error("Erreur lors de la synchronisation du modèle Info:", error);
+    }
+})();
+
+
+module.exports = Info;
