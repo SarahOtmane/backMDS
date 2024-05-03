46f8161c5487b502a67b1019cdbaae1ced074f47
SarahOtmane <sarahotmane02@gmail.com>
2024-05-04 01:20:15 +0200

    ajout de la fonction createAnInfo au route

---
+++ src/routes/infoRoute.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const infoController = require('../controllers/infoController.js');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
+
+
+
+router
+    .route('/')
+    .all(jwtMiddleware.verifyToken)
+    .post(infoController.createAnInfo)
+
+
+
+module.exports = router;
