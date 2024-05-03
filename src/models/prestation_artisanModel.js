b8152997896ff61c8d55845d2aed1966a2d6b673
SarahOtmane <sarahotmane02@gmail.com>
2024-05-04 00:37:20 +0200

    crea du model pour la table prestation_artisan

---
+++ src/models/prestation_artisanModel.js
@@ -0,0 +1,40 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: "db",
+    dialect: "mysql"
+});
+
+
+const prestation_artisan = sequelize.define('prestation_artisan', {
+    id_artisan: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    id_prestation: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+}, {
+    tableName: 'prestation_artisans',
+    timestamps: true,
+    underscored: true
+});
+
+// Synchronisation du modèle avec la base de données
+(async () => {
+    try {
+        //ne pas forcer a supp et recréer la table
+        await prestation_artisan.sync({ force: false });
+        console.log("Modèle prestation_artisan synchronisé avec la base de données.");
+    } catch (error) {
+        console.error("Erreur lors de la synchronisation du modèle prestation_artisan:", error);
+    }
+})();
+
+
+module.exports = prestation_artisan;
