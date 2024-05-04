8cf806a7402490cd15e5c48aefe6b2dd9a0750b3
SarahOtmane <sarahotmane02@gmail.com>
2024-05-04 23:38:06 +0200

    fonction qui permet a l admin de modif les details d une presta : putAPresta

--- src/controllers/prestationController.js
+++ src/controllers/prestationController.js
@@ -54,3 +54,39 @@ exports.getAPrestation = async (req, res) => {
         res.status(500).json({message: "Erreur lors du traitement des données."});
     }
 };
+
+
+
+
+/**********************************************************
+            MÉTHODE POUR MODIFIER UNE PRESTA (ADMIN)
+**********************************************************/
+/*
+    Fonction qui permet de modifier une presta
+
+    Les vérifications : 
+        - Vérifier que la presta existe
+
+*/
+exports.putAPresta = async (req, res) => {
+    try {
+        const presta = await Prestation.findOne({ where: { id: req.params.id } });
+
+        if(!presta){
+            return res.status(404).json({ message: 'Prestation non trouvé.' });
+        }
+
+        await presta.update({ 
+            categorie: req.body.categorie,
+            clothType: req.body.clothType,
+            reparationType: req.body.reparationType,
+            priceSuggested: req.body.priceSuggested,
+        });
+
+        
+        res.status(201).json({ message: 'Prestation mise à jour avec succès.' });
+
+    } catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
