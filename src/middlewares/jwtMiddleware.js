a900c8553f6d5043dbdfb68cca6e7f55bebdce76
SarahOtmane <sarahotmane02@gmail.com>
2024-05-04 20:40:08 +0200

    function verifyToken pour un user et une pour artisan

--- src/middlewares/jwtMiddleware.js
+++ src/middlewares/jwtMiddleware.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-//fonction pour vérifier les token
-exports.verifyToken = async(req, res, next) =>{
+//fonction pour vérifier les token d'un utilisateur
+exports.verifyTokenUser = async(req, res, next) =>{
     try {
         let token = req.headers['authorization'];
         if(token != undefined){
@@ -18,6 +18,35 @@ exports.verifyToken = async(req, res, next) =>{
             })
 
             req.user = payload;
+            next();
+        }else{
+            res.status(403).json({message: "Accès interdit: token manquant"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(403).json({message: "Accès interdit: token invalide"});
+    }
+}
+
+
+
+//fonction pour vérifier les token d'un utilisateur
+exports.verifyTokenArtisan = async(req, res, next) =>{
+    try {
+        let token = req.headers['authorization'];
+        if(token != undefined){
+            
+            const payload = await new Promise((resolve, reject) =>{
+                jwt.verify(token, process.env.JWT_KEY, (error, decoded) =>{
+                    if(error){
+                        reject(error);
+                    }else{
+                        resolve(decoded);
+                    }
+                })
+            })
+
+            req.artisan = payload;
             next();
         }else{
             res.status(403).json({message: "Accès interdit: token manquant"});
