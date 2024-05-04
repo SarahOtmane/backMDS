2aabaef7733ce4431add8bcc223d825337417470
SarahOtmane <sarahotmane02@gmail.com>
2024-05-05 00:36:41 +0200

    schema de la table info

--- src/docs/swagger/infoSwagger.js
+++ src/docs/swagger/infoSwagger.js
@@ -4,3 +4,26 @@
  *   name: Info
  *   description: CRUD pour la gestion des info
  */
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Info:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *         - content
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: ID de l'info
+ *         name:
+ *           type: string
+ *           description: Titre / Nom de l'info
+ *         content:
+ *           type: string
+ *           description: Contenu de l'info
+ */
