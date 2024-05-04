7023e419a1a21e1ed52e0ab7413950cc4cb1131a
SarahOtmane <sarahotmane02@gmail.com>
2024-05-05 00:39:47 +0200

    getAllInfo

--- src/docs/swagger/infoSwagger.js
+++ src/docs/swagger/infoSwagger.js
@@ -27,3 +27,29 @@
  *           type: string
  *           description: Contenu de l'info
  */
+
+
+
+
+/**
+ * @swagger
+ * /infos:
+ *   get:
+ *     summary: Récupérer toutes les infos en BDD
+ *     tags: [Info]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       201:
+ *         description: Informations récupérées avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Info'
+ *       403:
+ *         description: token manquant ou invalide
+ *       404:
+ *         description: Aucune info trouvée
+ *       500:
+ *         description: Erreur interne du serveur
+ */
