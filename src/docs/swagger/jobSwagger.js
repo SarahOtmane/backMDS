cc0419b6e04c1ad9e8b703d3035b8ad057973786
SarahOtmane <sarahotmane02@gmail.com>
2024-05-05 23:40:50 +0200

    schema de la table job

---
+++ src/docs/swagger/jobSwagger.js
@@ -0,0 +1,25 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Job
+ *   description: CRUD pour la gestion des job
+ */
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Info:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: ID du job
+ *         name:
+ *           type: string
+ *           description: Titre / Nom de la catégorie d'artisans
+ */
