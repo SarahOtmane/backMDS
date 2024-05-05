b765b3e08ee6761327a1f27e789e7b1c36df8d76
SarahOtmane <sarahotmane02@gmail.com>
2024-05-05 23:57:15 +0200

    doc swagger de  putAPresta et deleteAPresta

--- src/docs/swagger/prestationSwagger.js
+++ src/docs/swagger/prestationSwagger.js
@@ -130,3 +130,75 @@
  *       500:
  *         description: Erreur interne du serveur
  */
+
+
+
+
+
+
+/**
+ * @swagger
+ * /prestations/{id}:
+ *   put:
+ *     summary: Modifier une prestation par l'admin
+ *     tags: [Prestation]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la prestation à récupérer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               categorie:
+ *                 type: string
+ *               clothType:
+ *                 type: string
+ *               reparationType:
+ *                 type: string
+ *               priceSuggested:
+ *                 type: string
+ *             required:
+ *               - categorie
+ *               - clothType
+ *               - reparationType
+ *               - priceSuggested
+ *     responses:
+ *       201:
+ *         description: Prestation mise à jour avec succès
+ *       403:
+ *         description: token manquant ou invalide
+ *       404:
+ *         description: Aucune prestation trouvée
+ *       500:
+ *         description: Erreur interne du serveur
+ *   delete:
+ *     summary: Supprimer une prestation par l'admin
+ *     tags: [Prestation]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la prestation à récupérer
+ *     responses:
+ *       201:
+ *         description: Prestation supprimée avec succès
+ *       403:
+ *         description: token manquant ou invalide
+ *       404:
+ *         description: Aucune inprestationfo trouvée
+ *       500:
+ *         description: Erreur interne du serveur
+ */
