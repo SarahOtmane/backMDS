20b1f777b6f95d5452dd21429180a915577e96ef
SarahOtmane <sarahotmane02@gmail.com>
2024-05-05 23:42:46 +0200

    doc swagger de getAJob

--- src/docs/swagger/jobSwagger.js
+++ src/docs/swagger/jobSwagger.js
@@ -46,3 +46,33 @@
  *       500:
  *         description: Erreur interne du serveur
  */
+
+
+
+/**
+ * @swagger
+ * /jobs/{id}:
+ *   get:
+ *     summary: Récupérer un job
+ *     tags: [Job]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID du job à récupérer
+ *     responses:
+ *       201:
+ *         description: Job récupéré avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Job'
+ *       403:
+ *         description: token manquant ou invalide
+ *       404:
+ *         description: Aucun job trouvée
+ *       500:
+ *         description: Erreur interne du serveur
+ */
