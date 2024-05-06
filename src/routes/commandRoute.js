217f2bab2e431b735dfe9a69d6e76d120af409c0
SarahOtmane <sarahotmane02@gmail.com>
2024-05-06 21:45:13 +0200

    config route pour la fonction createACommand

--- src/routes/commandRoute.js
+++ src/routes/commandRoute.js
@@ -2,3 +2,10 @@ const express = require('express');
 const router = express.Router();
 const commandController = require('../controllers/commandController');
 const jwtMiddleware = require('../middlewares/jwtMiddleware');
+
+
+router
+    .route('/:id_artisan')
+    .all(jwtMiddleware.verifyTokenUser)
+    .post(commandController.createACommand)
+
