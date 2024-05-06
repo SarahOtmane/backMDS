adc879a106aa3bdc89abe26b90569c6d992c47f2
SarahOtmane <sarahotmane02@gmail.com>
2024-05-06 21:55:00 +0200

    getAllCommand => fonction qui permet a l admin de lister toutes les commandes

--- src/controllers/commandController.js
+++ src/controllers/commandController.js
@@ -92,3 +92,31 @@ exports.putACommand = async (req, res) => {
         res.status(500).json({message: "Erreur lors du traitement des données."});
     }
 };
+
+
+
+
+/*****************************************************************
+            MÉTHODE POUR LISTER TOUTES LES COMMANDES (ADMIN)
+*****************************************************************/
+/*
+    Fonction qui permet de lister toutes les commandes
+
+    Les vérifications : 
+        - Vérifier que les commandes existent
+
+*/
+exports.getAllCommand = async (req, res) => {
+    try {
+        const commands = await Command.findAll();
+        
+        if (!commands) {
+            return res.status(404).json({ message: 'Auncune commande trouvée.' });
+        }
+
+        res.status(201).json(commands);
+
+    } catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
