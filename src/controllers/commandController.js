6f0a9fc76447aa34463c3b8f6b7d385d7f270a56
SarahOtmane <sarahotmane02@gmail.com>
2024-05-06 21:58:33 +0200

    getCommandOfArtisan

--- src/controllers/commandController.js
+++ src/controllers/commandController.js
@@ -120,3 +120,32 @@ exports.getAllCommand = async (req, res) => {
         res.status(500).json({message: "Erreur lors du traitement des données."});
     }
 };
+
+
+
+
+
+/*****************************************************************
+            MÉTHODE POUR LISTER TOUTES LES COMMANDES ARTISAN
+*****************************************************************/
+/*
+    Fonction qui permet de lister toutes les commandes d'un artisan
+
+    Les vérifications : 
+        - Vérifier que les commandes existent
+
+*/
+exports.getCommandOfArtisan = async (req, res) => {
+    try {
+        const commands = await Command.findAll({where: {id: req.artisan.id}});
+        
+        if (!commands) {
+            return res.status(404).json({ message: 'Auncune commande trouvée.' });
+        }
+
+        res.status(201).json(commands);
+
+    } catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
