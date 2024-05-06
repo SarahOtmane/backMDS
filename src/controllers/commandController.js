ac19f59c1243f3c2c1455d614c1fee7518b803d5
SarahOtmane <sarahotmane02@gmail.com>
2024-05-06 15:21:05 +0200

    prem version de createACommand

--- src/controllers/commandController.js
+++ src/controllers/commandController.js
@@ -1,1 +1,39 @@
 const Command = require('../models/commandModel');
+const Artisan = require('../models/artisanModel');
+
+
+
+
+/**********************************************************
+            MÉTHODE POUR CREER UNE COMMANDE
+**********************************************************/
+/*
+    Fonction qui permet à un user de créer une commande
+
+    Les vérifications : 
+        - l existance de l artisan
+
+*/
+exports.createAnInfo = async (req, res) => {
+    try {
+        const existingArtisan = await Artisan.findOne({ where: { id: req.params.id_artisan } });
+        if (!existingArtisan) {
+            return res.status(401).json({ message: 'L\'artisan n\'existe plus en base de données.' });
+        }
+
+        let NewCommand = await Command.create({
+            name: req.body.name,
+            picture: req.body.picture,
+            dateFinished: false,
+            id_artisan: req.params.id_artisan,
+            id_user: req.user.id,
+        });
+
+        res.status(201).json({ 
+            message: `Info créé avec succès. Le nom : ${NewCommand.name}` 
+        });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
