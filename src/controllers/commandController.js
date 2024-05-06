bf652d5d5eb151c068eae0d9e4e0e720994d12b3
SarahOtmane <sarahotmane02@gmail.com>
2024-05-06 22:00:32 +0200

    getCommandOfUser

--- src/controllers/commandController.js
+++ src/controllers/commandController.js
@@ -149,3 +149,31 @@ exports.getCommandOfArtisan = async (req, res) => {
         res.status(500).json({message: "Erreur lors du traitement des données."});
     }
 };
+
+
+
+
+/*****************************************************************
+            MÉTHODE POUR LISTER TOUTES LES COMMANDES USER
+*****************************************************************/
+/*
+    Fonction qui permet de lister toutes les commandes d'un user
+
+    Les vérifications : 
+        - Vérifier que les commandes existent
+
+*/
+exports.getCommandOfUser = async (req, res) => {
+    try {
+        const commands = await Command.findAll({where: {id: req.user.id}});
+        
+        if (!commands) {
+            return res.status(404).json({ message: 'Auncune commande trouvée.' });
+        }
+
+        res.status(201).json(commands);
+
+    } catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
