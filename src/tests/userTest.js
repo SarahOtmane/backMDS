ab589c8df374510b80cb137f79679fedc03d5956
SarahOtmane <sarahotmane02@gmail.com>
2024-05-07 09:40:15 +0200

    test unitaire de la methode registerAUser okay

--- src/tests/userTest.js
+++ src/tests/userTest.js
@@ -6,10 +6,10 @@ const User = require('../models/userModel');
 describe('User Controller', () => {
 
     //Test de la méthode registerAUser
-    describe('POST users/register', ()=>{
+    describe('POST /users/register', ()=>{
         it('should register a new user', async () => {
             const res = await request(app)
-                .post('users/register')
+                .post('/users/register')
                 .send({
                     email: 'sarah@user.com',
                     password: 'sarah',
@@ -24,12 +24,12 @@ describe('User Controller', () => {
                     postalCode: 75011
                 });
             expect(res.statusCode).toEqual(201);
-            expect(res.json).toEqual({message: `Utilisateur créé avec succès. L'email : sarah@user.com ` });
+            expect(res.body).toEqual({message: `Utilisateur créé avec succès. L'email : sarah@user.com ` });
         });
 
-        it('should return 401 if email already exists', async() =>{
+        it('should return 409 if email already exists', async() =>{
             const res = await request(app)
-                .post('users/register')
+                .post('/users/register')
                 .send({
                     email: 'sarah@user.com',
                     password: 'sarah',
@@ -43,8 +43,8 @@ describe('User Controller', () => {
                     country: 'France',
                     postalCode: 75011
                 });
-            expect(res.statusCode).toEqual(401);
-            expect(res.json).toEqual({message: `Cet email existe déjà.` });
+            expect(res.statusCode).toEqual(409);
+            expect(res.body).toEqual({message: `Cet email existe déjà.` });
         });
 
         it('should return 401 if the role is admin', async() =>{
@@ -64,10 +64,28 @@ describe('User Controller', () => {
                     postalCode: 75011
                 });
             expect(res.statusCode).toEqual(401);
-            expect(res.json).toEqual({message: `Vous ne pouvez pas créer un utilisateur avec le rôle admin.` });
-        })
+            expect(res.body).toEqual({message: `Vous ne pouvez pas créer un utilisateur avec le rôle admin.` });
+        });
+
+        it('should return 500 if error in server', async() =>{
+            const res = await request(app)
+                .post('/users/register')
+                .send({
+                    email: 'sarah@user.com',
+                    password: 'sarah',
+                    role: 'user',
+                    firstname: 'sarah',
+                    lastname: 'otmane',
+                    mobile: '0606060606',
+                    subscribeNewsletter: false,
+                    streetAdress: '40 chemin vert',
+                    city: 'Paris',
+                    country: 'France',
+                    postalCode: 75011
+                });
+            expect(res.statusCode).toEqual(500);
+            expect(res.body).toEqual({message: `Erreur serveur` });
+        });
     });
-
-
-
+    
 });
