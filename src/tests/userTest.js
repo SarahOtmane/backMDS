2cb34b628bb401853fc103ca8e6b3c7bb8a9e076
SarahOtmane <sarahotmane02@gmail.com>
2024-05-07 09:28:06 +0200

    test unitaire de la fonction registerAUser

--- src/tests/userTest.js
+++ src/tests/userTest.js
@@ -3,3 +3,31 @@ const app = require('../app');
 const User = require('../models/userModel');
 
 
+describe('User Controller', () => {
+
+    //Test de la méthode registerAUser
+    describe('POST users/register', ()=>{
+        it('should register a new user', async () => {
+            const res = await request(app)
+                .post('users/register')
+                .send({
+                    email: 'sarah@user.com',
+                    password: 'sarah',
+                    role: 'user',
+                    firstname: 'sarah',
+                    lastname: 'otmane',
+                    mobile: '0606060606',
+                    subscribeNewsletter: false,
+                    streetAdress: '40 chemin vert',
+                    city: 'Paris',
+                    country: 'France',
+                    postalCode: 75011
+                });
+            expect(res.statusCode).toEqual(201);
+            expect(res.json).toEqual({message: `Utilisateur créé avec succès. L'email : sarah@user.com ` });
+        });
+    });
+
+
+    
+});
