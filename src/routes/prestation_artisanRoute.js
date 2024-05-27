6896f1c63aebd5a61609087ea970741f967603f2
SarahOtmane <sarahotmane02@gmail.com>
2024-05-27 22:27:06 +0200

    createAPrestaArtisan : config route

--- src/routes/prestation_artisanRoute.js
+++ src/routes/prestation_artisanRoute.js
@@ -2,3 +2,15 @@ const express = require('express');
 const router = express.Router();
 const jwtMiddleware = require('../middlewares/jwtMiddleware');
 const prestation_artisanController = require('../controllers/prestation_artisanController');
+
+
+
+router
+    .route('/')
+    .all(jwtMiddleware.verifyTokenArtisan)
+    .post(prestation_artisanController.createAPrestaArtisan)
+
+
+
+export default router;
+
