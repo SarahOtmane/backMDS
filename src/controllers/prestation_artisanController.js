176547693ae9a9b5592f6643e1a27d917721c38a
SarahOtmane <sarahotmane02@gmail.com>
2024-05-27 22:44:45 +0200

    updateAPrestaArtisan

--- src/controllers/prestation_artisanController.js
+++ src/controllers/prestation_artisanController.js
@@ -9,7 +9,7 @@ const Prestation = require('../models/prestationModel');
             MÉTHODE POUR CREER UNE COMMANDE
 **********************************************************/
 /*
-    Fonction qui permet à un user de créer une commande
+    Fonction qui permet de creer la table intermediaire
 
     Les vérifications : 
         - l existance de l artisan
@@ -38,6 +38,52 @@ exports.createAPrestaArtisan = async (req, res) => {
 
         res.status(201).json({ 
             message: `Prestation_Artisan créée avec succès.` 
+        });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
+
+
+
+
+/**********************************************************
+            MÉTHODE POUR MODIFIER UNE COMMANDE
+**********************************************************/
+/*
+    Fonction qui permet d'update la table intermediaire
+
+    Les vérifications : 
+        - l existance de l artisan
+        - l existance de la prestation
+
+*/
+exports.updateAPrestaArtisan = async (req, res) => {
+    try {
+        const artisan = await Artisan.findOne({ where: { id: req.artisan.id} });
+        if(!artisan){
+            return res.status(404).json({ message: 'Artisan non trouvé.' });
+        }
+
+        const existingPresta = await Prestation.findOne({where: {
+            categorie: req.body.categorie,
+            clothType: req.body.clothType,
+            reparationType: req.body.reparationType
+        }})
+        if(!existingPresta) return res.status(404).json({message: 'La prestation n\'existe plys en base de donnés'});
+
+        const existingPrestaArtisan = await Prestation_Artisan.findOne({where:{
+            id_artisan: artisan.id,
+            id_prestation: existingPresta.id
+        }});
+
+        await existingPrestaArtisan.update({
+            price: req.body.price,
+        });
+
+        res.status(201).json({ 
+            message: `Prestation_Artisan update avec succès.` 
         });
     } 
     catch (error) {
