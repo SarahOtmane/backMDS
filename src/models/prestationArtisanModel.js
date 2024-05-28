5995e413ec8ebf02a4121cf06f1ab85b94b8d7ab
SarahOtmane <sarahotmane02@gmail.com>
2024-05-28 12:08:09 +0200

    update

--- src/models/prestationArtisanModel.js
+++ src/models/prestationArtisanModel.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 
-const PrestationArtisan = sequelize.define('PrestationArtisan', {
+const Prestationartisan = sequelize.define('Prestationartisan', {
     id_artisan: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -20,7 +20,7 @@ const PrestationArtisan = sequelize.define('PrestationArtisan', {
         allowNull: false,
     },
 }, {
-    tableName: 'prestationArtisans',
+    tableName: 'Prestationartisans',
     timestamps: true,
     underscored: true
 });
@@ -30,17 +30,17 @@ const PrestationArtisan = sequelize.define('PrestationArtisan', {
 const Prestation = require('./prestationModel');
 const Artisan = require('./artisanModel');
 
-Prestation.hasMany(PrestationArtisan, {
+Prestation.hasMany(Prestationartisan, {
     foreignKey: "id_prestation",
 });
-PrestationArtisan.belongsTo(Prestation, {
+Prestationartisan.belongsTo(Prestation, {
     foreignKey: "id_prestation",
 });
 
-Artisan.hasMany(PrestationArtisan, {
+Artisan.hasMany(Prestationartisan, {
     foreignKey: "id_artisan",
 });
-PrestationArtisan.belongsTo(Artisan, {
+Prestationartisan.belongsTo(Artisan, {
     foreignKey: "id_artisan",
 });
 
@@ -49,12 +49,12 @@ PrestationArtisan.belongsTo(Artisan, {
 (async () => {
     try {
         //ne pas forcer a supp et recréer la table
-        await PrestationArtisan.sync({ force: false });
-        console.log("Modèle prestation_artisan synchronisé avec la base de données.");
+        await Prestationartisan.sync({ force: false });
+        console.log("Modèle prestationartisan synchronisé avec la base de données.");
     } catch (error) {
         console.error("Erreur lors de la synchronisation du modèle prestation_artisan:", error);
     }
 })();
 
 
-module.exports = PrestationArtisan;
+module.exports = Prestationartisan;
