282671f9dd3eece8b6427c0a7c527d2385456501
SarahOtmane <sarahotmane02@gmail.com>
2024-05-29 21:46:36 +0200

    fichier controller de la table newsletter

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sequelize = require("sequelize");
 const cors = require('cors');
 const app = express();
-const port = 3004;
+const port = 3003;
 
 
 // Configuration de la base de données
