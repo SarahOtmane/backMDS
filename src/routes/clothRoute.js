137bf77d74031334a3b41107a3d82f5bb99d8665
SarahOtmane <sarahotmane02@gmail.com>
2024-05-30 11:50:00 +0200

    config fichier Cloth Route

---
+++ src/routes/clothRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
+const clothController = require('../controllers/clothConntroller');
+
+
+router
+    .route('/')
+
+
+
+
+module.exports = router;
