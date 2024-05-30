bb497ea307380d7b5c3ad067d7de48d6bac5f4d7
SarahOtmane <sarahotmane02@gmail.com>
2024-05-30 11:54:12 +0200

    createAJob

--- src/controllers/clothConntroller.js
+++ src/controllers/clothConntroller.js
@@ -1,1 +1,34 @@
 const Cloth = require('../models/clothModel');
+
+
+/**********************************************************
+            MÉTHODE POUR CREER UN CLOTH (ADMIN)
+**********************************************************/
+/*
+    Fonction qui permet à l'admin de créer un cloth
+
+    Les vérifications : 
+        - l existance du cloth
+
+*/
+exports.createAJob = async (req, res) => {
+    try {
+        const existingCloth = await Cloth.findOne({ where: { 
+            categorie: req.body.categorie,
+            clothType: req.body.clothType,
+            id_job: req.body.id_job
+        } });
+        if (existingCloth) {
+            return res.status(401).json({ message: 'Cet habit existe déjà.' });
+        }
+
+        await Cloth.create(req.body);
+
+        res.status(201).json({ 
+            message: `Habit créé avec succès.` 
+        });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
