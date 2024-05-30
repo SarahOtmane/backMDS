94d6e7ba6028a444065b4632ff13669700f03fab
SarahOtmane <sarahotmane02@gmail.com>
2024-05-30 12:00:56 +0200

    putACloth : config route

--- src/routes/clothRoute.js
+++ src/routes/clothRoute.js
@@ -4,9 +4,27 @@ const jwtMiddleware = require('../middlewares/jwtMiddleware');
 const clothController = require('../controllers/clothConntroller');
 
 
+
+
+
+
+
+
+
+/**********************************************************
+            ROUTES UNIQUEMENT POUR LES ADMINS
+**********************************************************/ 
+
 router
     .route('/')
+    .all(jwtMiddleware.isAdmin)
+    .post(clothController.createACloth)
 
+router
+    .route('/:id_job')
+    .all(jwtMiddleware.isAdmin)
+    .put(clothController.putACloth)
+    .delete(clothController.deleteAJob);
 
 
 
