cff4096cd0c9dbec093c1b8e25fd88041c277ba2
SarahOtmane <sarahotmane02@gmail.com>
2024-05-30 12:00:46 +0200

    putACloth

--- src/controllers/clothConntroller.js
+++ src/controllers/clothConntroller.js
@@ -11,7 +11,7 @@ const Cloth = require('../models/clothModel');
         - l existance du cloth
 
 */
-exports.createAJob = async (req, res) => {
+exports.createACloth = async (req, res) => {
     try {
         const existingCloth = await Cloth.findOne({ where: { 
             categorie: req.body.categorie,
@@ -32,3 +32,36 @@ exports.createAJob = async (req, res) => {
         res.status(500).json({message: "Erreur lors du traitement des données."});
     }
 };
+
+
+
+/**********************************************************
+            MÉTHODE POUR UPDATE UN CLOTH (ADMIN)
+**********************************************************/
+/*
+    Fonction qui permet à l'admin de modifier un cloth
+
+    Les vérifications : 
+        - l existance du cloth
+
+*/
+exports.putACloth = async (req, res) => {
+    try {
+        const existingCloth = await Cloth.findOne({ where: { id: req.params.id_cloth } });
+        if (!existingCloth) {
+            return res.status(401).json({ message: 'Cet habit non trouvé.' });
+        }
+
+        await existingCloth.update({ 
+            categorie: req.body.categorie,
+            clothType: req.body.clothType,
+            id_job: req.body.id_job
+        });
+
+        res.status(201).json({ message: 'Cloth mis à jour avec succès.' });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
+
