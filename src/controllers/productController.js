708423705c8ad9712056cae4b931c6a751af4ccd
SarahOtmane <sarahotmane02@gmail.com>
2024-05-30 14:28:21 +0200

    update file name to productController

---
+++ src/controllers/productController.js
@@ -0,0 +1,173 @@
+const Product = require('../models/productModel');
+const Artisan = require('../models/artisanModel');
+const Prestation = require('../models/prestationModel');
+const Cloth = require('../models/clothModel');
+
+
+
+
+/**********************************************************
+            MÉTHODE POUR CREER UN PRODUCT
+**********************************************************/
+/*
+    Fonction qui permet de creer une presta d un artisan
+
+    Les vérifications : 
+        - l existance de l artisan
+        - l existance de la prestation
+
+*/
+exports.createAProduct = async (req, res) => {
+    try {
+        const artisan = await Artisan.findOne({ where: { id: req.artisan.id} });
+        if(!artisan){
+            return res.status(404).json({ message: 'Artisan non trouvé.' });
+        }
+
+        const cloth = await Cloth.findOne({where: {
+            categorie: req.body.categorie,
+            clothType: req.body.clothType,
+        }})
+        if(!cloth){
+            return res.status(404).json({ message: 'Habit non trouvé.' });
+        }
+
+        const existingPresta = await Prestation.findOne({where: {
+            reparationType: req.body.reparationType
+        }})
+        if(!existingPresta) return res.status(404).json({message: 'La prestation n\'existe plus en base de donnés'})
+
+        await Product.create({
+            price: req.body.price,
+            id_artisan: artisan.id,
+            id_prestation: existingPresta.id,
+            id_cloth: cloth.id
+        });
+
+        res.status(201).json({ 
+            message: `Product créée avec succès.` 
+        });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
+
+
+
+
+/**********************************************************
+            MÉTHODE POUR MODIFIER UNE PRESTA_ARTISAN
+**********************************************************/
+/*
+    Fonction qui permet d'update le price d une presta dun artisan
+
+    Les vérifications : 
+        - l existance de l artisan
+        - l existance de la prestation
+
+*/
+exports.updateAProduct = async (req, res) => {
+    try {
+        const product = await Product.findOne({where: {id: req.params.id_product}});
+        if(!product){
+            return res.status(404).json({ message: 'Produit non trouvé.' });
+        }
+        const artisan = await Artisan.findOne({ where: { id: req.artisan.id} });
+        if(!artisan){
+            return res.status(404).json({ message: 'Artisan non trouvé.' });
+        }
+
+        const cloth = await Cloth.findOne({where: {
+            categorie: req.body.categorie,
+            clothType: req.body.clothType,
+        }})
+        if(!cloth){
+            return res.status(404).json({ message: 'Habit non trouvé.' });
+        }
+
+        const existingPresta = await Prestation.findOne({where: {
+            reparationType: req.body.reparationType
+        }})
+        if(!existingPresta) return res.status(404).json({message: 'La prestation n\'existe plus en base de donnés'})
+
+        await product.update({
+            price: req.body.price,
+            id_artisan: artisan.id,
+            id_prestation: existingPresta.id,
+            id_cloth: cloth.id
+        });
+
+        res.status(201).json({ 
+            message: `Product update avec succès.` 
+        });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
+
+
+
+/**********************************************************
+            MÉTHODE POUR SUPRIMER UN PRODUCT 
+**********************************************************/
+/*
+    Fonction qui permet de supprimer un product
+
+    Les vérifications : 
+        - l existance de l artisan
+        - l existance de la prestation
+
+*/
+exports.deleteAProduct = async (req, res) => {
+    try {
+        const deleteProduct = await Product.destroy({where: { 
+            id: req.params.id_product
+        }});
+        
+        if (!deleteProduct) {
+            return res.status(404).json({ message: 'Product non trouvé.' });
+        }
+
+        res.status(201).json({ message: 'Product supprimé avec succès.' });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
+
+
+
+/**********************************************************
+            MÉTHODE POUR LISTER TOUS LES PRODUCTS
+**********************************************************/
+/*
+    Fonction qui permet de lister les products
+
+    Les vérifications : 
+        - l existance de l artisan
+        - l existance de la prestation
+
+*/
+
+exports.getAllProductsArtisan = async (req, res) => {
+    try {
+        const artisan = await Artisan.findOne({where: {id: req.artisan.id}});
+        if(!artisan){
+            return res.status(404).json({ message: 'Artisan non trouvé.' });
+        }
+
+        const products = await Product.findOne({where: {id_artisan: artisan.id}});
+        if(!products){
+            return res.status(404).json({ message: 'Auncun product trouvé.' });
+        }
+
+        for(let index = 0; index <products.length; index++){
+            
+        }
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
