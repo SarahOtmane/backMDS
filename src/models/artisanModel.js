537f20abfaac151e8fde9c298f30a93b0a0eb535
SarahOtmane <sarahotmane02@gmail.com>
2024-06-29 18:59:49 +0200

    update artisanModel

--- src/models/artisanModel.js
+++ src/models/artisanModel.js
@@ -1,35 +1,11 @@
-const { Sequelize, DataTypes } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "db",
-    dialect: "mysql"
-});
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/connectBdd');
 
 const Artisan = sequelize.define('Artisan', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
-    },
-    email: {
-        type: DataTypes.STRING,
-        unique: true,
-    },
-    firstname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    lastname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    mobile: {
-        type: DataTypes.STRING,
-        allowNull: false
     },
     acceptNewOrder: {
         type: DataTypes.BOOLEAN,
@@ -39,30 +15,23 @@ const Artisan = sequelize.define('Artisan', {
             isIn: [[false, true]]
         }
     },
-    streetAdress: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    city: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    country: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    postalCode: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-    },
     siret: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    numeroTVA: {
+    tva: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    picture: {
         type: DataTypes.STRING,
         allowNull: false
     }
+
 }, {
     tableName: 'artisans',
     timestamps: true,
@@ -71,10 +40,10 @@ const Artisan = sequelize.define('Artisan', {
 
 const Job = require('./jobModel');
     Job.hasMany(Artisan, {
-        foreignKey: 'id_job',
+        foreignKey: 'name_job',
     });
     Artisan.belongsTo(Job, {
-        foreignKey: 'id_job',
+        foreignKey: 'name_job',
     });
 
 module.exports = Artisan;
