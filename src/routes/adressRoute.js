94901ac3dd17565d8dffd953d1232c508b4f8532
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 12:43:22 +0200

    remove all routes

--- src/routes/adressRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adressController = require('../controllers/adressController');
-const jwtMiddleware = require('../middlewares/jwtMiddleware');
-
-
-router
-    .route('/register')
-    .post(adressController.createAnAdress)
