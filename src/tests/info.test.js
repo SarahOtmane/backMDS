e9e3c5fd5083606f78480a3d98c72c65982170de
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 23:38:39 +0200

    info test okay

--- src/tests/info.test.js
+++ src/tests/info.test.js
@@ -53,30 +53,30 @@ describe('Info controller', () => {
         });
     });
 
-    describe('GET /infos/:id_info', () => {
+    describe('GET /infos/:name_info', () => {
         it('should return 201 when getting the details of info', async () => {
             const info = await Info.create({name: 'Test1', content: 'Content1' });
 
             const { statusCode, body } = await supertest(app)
-                .get(`/infos/${info.id}`);
+                .get(`/infos/${info.name}`);
 
             expect(statusCode).toBe(201);
         });
 
         it('should return 404 when the info is not found', async () => {
             const { statusCode, body } = await supertest(app)
-                .get(`/infos/9999`);
+                .get(`/infos/test`);
 
             expect(statusCode).toBe(404);
         });
     });
 
-    describe('PUT /infos/:id_info', () => {
+    describe('PUT /infos/:name_info', () => {
         it('should return 201 when updating the info', async () => {
             const info = await Info.create({name: 'Test1', content: 'Content1' });
 
             const { statusCode, body } = await supertest(app)
-                .put(`/infos/${info.id}`)
+                .put(`/infos/${info.name}`)
                 .send({
                     name: 'Test1', 
                     content: 'Content3'
@@ -87,7 +87,7 @@ describe('Info controller', () => {
 
         it('should return 404 when the info is not found', async () => {
             const { statusCode, body } = await supertest(app)
-                .put(`/infos/9999`)
+                .put(`/infos/teeest`)
                 .send({
                     name: 'Test1', 
                     content: 'Content3'
@@ -97,19 +97,19 @@ describe('Info controller', () => {
         });
     });
 
-    describe('DELETE /infos/:id_info', () => {
+    describe('DELETE /infos/:name_info', () => {
         it('should return 201 when deleting the info', async () => {
             const info = await Info.create({name: 'Test1', content: 'Content1' });
 
             const { statusCode, body } = await supertest(app)
-                .delete(`/infos/${info.id}`);
+                .delete(`/infos/${info.name}`);
 
             expect(statusCode).toBe(201);
         });
 
         it('should return 404 when the info is found', async () => {
             const { statusCode, body } = await supertest(app)
-                .delete('/info/9999')
+                .delete('/info/teeeeest')
 
             expect(statusCode).toBe(404);
         });
