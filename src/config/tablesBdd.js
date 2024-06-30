49f09f749585ae4a493e23cc6ebf3c3b7f4a7ade
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 16:52:11 +0200

    creer les tables

--- src/config/tablesBdd.js
+++ src/config/tablesBdd.js
@@ -1,7 +1,19 @@
+const Info = require('./models/infoModel.js');
+const Job = require('./models/jobModel.js');
+const Newsletter = require('./models/newsletterModel.js');
+const Cloth = require('./models/clothModel.js');
+const Prestation = require('./models/prestationModel.js');
+
 
 async function createTablesInOrder() {
     try {
-        
+        await Info.sync();
+        await Job.sync();
+        await Newsletter.sync();
+        await Cloth.sync();
+        await Prestation.sync();
+
+        console.log('Tables crées');
     } catch (error) {
         console.error("Erreur lors de la création des tables :", error);
     }
