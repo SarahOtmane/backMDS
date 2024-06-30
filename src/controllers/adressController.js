6fdc38c65bca0939414cef5f22216a53caca3248
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 12:32:11 +0200

    createAdress

---
+++ src/controllers/adressController.js
@@ -0,0 +1,36 @@
+const Adress = require('../models/adressModel');
+
+
+/**********************************************************
+            MÉTHODE POUR CREER UNE ADRESSE
+**********************************************************/
+/*
+    Fonction qui permet de créer une adresse
+
+    Les vérifications : 
+        - l existance de l'adresse
+
+*/
+exports.createAnAdress = async (req, res) => {
+    try {
+        const existingAdress = await Adress.findOne({
+            where: {
+                streetAdress : req.body.streetAdress,
+                city : req.body.city,
+                postalCode : req.body.postalCode,
+                country : req.body.country
+            }
+        });
+
+        if(existingAdress){
+            return res.status(401).json({ message: 'Cette adresse existe déjà.' });
+        }
+        await Adress.create(req.body);
+        res.status(201).json({ 
+            message: `Habit créé avec succès.` 
+        });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
