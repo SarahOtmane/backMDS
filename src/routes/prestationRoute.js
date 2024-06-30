798d2d48a135c3378d777e8f1cdb2d2e883b00ca
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 16:46:39 +0200

    presta route

---
+++ src/routes/prestationRoute.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
+const prestationController = require('../controllers/prestationController.js');
+
+
+router
+    .route('/')
+    .get(prestationController.getAllPresta)
+
+router
+    .route('/:id_prestation')
+    .get(prestationController.getAPrestation)
+
+router
+    .route('/job/:name_job')
+    .get(prestationController.getAllPrestaOfJob)
+
+
+    
+/**********************************************************
+            ROUTES UNIQUEMENT POUR LES ADMINS
+**********************************************************/
+router
+    .route('/')
+    .all(jwtMiddleware.isAdmin)
+    .post(prestationController.createAPrestation)
+
+router
+    .route('/:id_prestation')
+    .all(jwtMiddleware.isAdmin)
+    .put(prestationController.putAPresta)
+    .delete(prestationController.deleteAPresta)
+
+
+
+module.exports = router;
