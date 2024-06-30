73f2eb2010cf3b881fb5c3d5dc0ac457b7e9fe5c
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 22:20:59 +0200

    update getcloth

--- src/controllers/clothConntroller.js
+++ src/controllers/clothConntroller.js
@@ -14,7 +14,7 @@ const Job = require('../models/jobModel');
 */
 exports.createACloth = async (req, res) => {
     try {
-        const job = Job.findOne({where: {name: req.body.name_job}});
+        const job = await Job.findOne({where: {name: req.body.name_job}});
         if(!job){
             return res.status(400).json({error: "Job non trouvé"});
         }
@@ -22,13 +22,17 @@ exports.createACloth = async (req, res) => {
         const existingCloth = await Cloth.findOne({ where: { 
             category: req.body.category,
             clothType: req.body.clothType,
-            name_job: job.name
+            name_job: req.body.name_job
         } });
         if (existingCloth) {
             return res.status(401).json({ message: 'Cet habit existe déjà.' });
         }
 
-        await Cloth.create(req.body);
+        const newCloth = await Cloth.create({ 
+            category: req.body.category,
+            clothType: req.body.clothType,
+            name_job: req.body.name_job
+        });
 
         res.status(201).json({ 
             message: `Habit créé avec succès.` 
@@ -121,7 +125,7 @@ exports.getAllClothes = async (req, res) => {
         
         const clothes = await Cloth.findAll();
         
-        if (!clothes) {
+        if (clothes.length === 0) {
             return res.status(404).json({ message: 'Auncun cloth trouvé.' });
         }
 
@@ -154,7 +158,7 @@ exports.getAllClothesOfJob = async (req, res) => {
         }
         
         const clothes = await Cloth.findAll({where: {name_job : req.params.name_job}});
-        if (!clothes) {
+        if (clothes.length === 0) {
             return res.status(404).json({ message: 'Auncun cloth trouvé.' });
         }
 
