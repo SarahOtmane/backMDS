f6eec98afefe24d54234cc62ad7992009906926b
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 12:46:02 +0200

    fichier pour la construction des tables

---
+++ src/config/tablesBdd.js
@@ -0,0 +1,10 @@
+
+async function createTablesInOrder() {
+    try {
+        
+    } catch (error) {
+        console.error("Erreur lors de la création des tables :", error);
+    }
+}
+
+module.exports = createTablesInOrder;
