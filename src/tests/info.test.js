e0308b2b8200389ecd83651256eece21a5564140
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 23:35:40 +0200

    update params

--- src/tests/info.test.js
+++ src/tests/info.test.js
@@ -53,4 +53,66 @@ describe('Info controller', () => {
         });
     });
 
+    describe('GET /infos/:id_info', () => {
+        it('should return 201 when getting the details of info', async () => {
+            const info = await Info.create({name: 'Test1', content: 'Content1' });
+
+            const { statusCode, body } = await supertest(app)
+                .get(`/infos/${info.id}`);
+
+            expect(statusCode).toBe(201);
+        });
+
+        it('should return 404 when the info is not found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .get(`/infos/9999`);
+
+            expect(statusCode).toBe(404);
+        });
+    });
+
+    describe('PUT /infos/:id_info', () => {
+        it('should return 201 when updating the info', async () => {
+            const info = await Info.create({name: 'Test1', content: 'Content1' });
+
+            const { statusCode, body } = await supertest(app)
+                .put(`/infos/${info.id}`)
+                .send({
+                    name: 'Test1', 
+                    content: 'Content3'
+                })
+
+            expect(statusCode).toBe(201);
+        });
+
+        it('should return 404 when the info is not found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .put(`/infos/9999`)
+                .send({
+                    name: 'Test1', 
+                    content: 'Content3'
+                })
+
+            expect(statusCode).toBe(404);
+        });
+    });
+
+    describe('DELETE /infos/:id_info', () => {
+        it('should return 201 when deleting the info', async () => {
+            const info = await Info.create({name: 'Test1', content: 'Content1' });
+
+            const { statusCode, body } = await supertest(app)
+                .delete(`/infos/${info.id}`);
+
+            expect(statusCode).toBe(201);
+        });
+
+        it('should return 404 when the info is found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .delete('/info/9999')
+
+            expect(statusCode).toBe(404);
+        });
+    });
+
 });
