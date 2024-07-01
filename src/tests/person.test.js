475a5bcfc54d062917e7e4b68010d7fa38d013bf
SarahOtmane <sarahotmane02@gmail.com>
2024-07-01 09:59:11 +0200

    creer un fichier pour les tests de person

---
+++ src/tests/person.test.js
@@ -0,0 +1,14 @@
+const createServeur = require('../config/serveur');
+const Person = require('../models/personModel');
+const supertest = require('supertest');
+
+const app = createServeur();
+
+describe('Person controller ', () => {
+    
+    describe('POST /persons/user/register', () => {
+        
+    });
+    
+
+});
