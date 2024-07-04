73b7652d25b59ee5d6847c16b4b1d5f421ddc730
SarahOtmane <sarahotmane02@gmail.com>
2024-07-04 19:59:45 +0200

    createACommand

--- src/controllers/commandController.js
+++ src/controllers/commandController.js
@@ -3,6 +3,61 @@ const Command = require('../models/commandModel');
 const Product = require('../models/productModel');
 
 
+
+/**********************************************************
+            MÉTHODE POUR CREER UNE COMMANDE
+**********************************************************/
+/*
+    Fonction qui permet à un user de créer une commande
+
+    Les vérifications : 
+        - l existance de l artisan
+        - l existance de la prestation
+
+*/
+exports.createACommand = async (req, res) => {
+    try {
+        const existingArtisan = await Artisan.findOne({ where: { id: req.params.id_artisan } });
+        if (!existingArtisan) {
+            return res.status(404).json({ message: 'L\'artisan n\'existe plus en base de données.' });
+        }
+
+        const existingCloth = await Cloth.findOne({where: {
+            category: req.body.category,
+            clothType: req.body.clothType,
+            id_job: req.body.id_job
+        }})
+        if(!existingCloth) return res.status(404).json({message: 'L\'habit n\'existe plus en base de donnés'});
+
+        const existingPresta = await Prestation.findOne({where: {reparationType: req.body.reparationType}});
+        if(!existingPresta) return res.status(404).json({message: 'La prestation n\'existe plus en base de donnés'});
+
+        const existingProduct = await Product.findOne({where: {
+            id_artisan: existingArtisan.id,
+            id_prestation: existingPresta.id
+        }});
+        if(!existingProduct) return res.status(404).json({message: 'Le produit n\'existe déjà'});
+
+        await Command.create({
+            name: req.body.name,
+            picture: req.body.picture,
+            dateFinished: null,
+            id_user: req.user.id,
+            id_product: existingProduct.id,
+            id_cloth: existingCloth.id,
+            comment: req.body.comment
+        });
+
+        res.status(201).json({ 
+            message: `Commande créée avec succès.` 
+        });
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
+
+
 /*****************************************************************
             MÉTHODE POUR LISTER TOUTES LES COMMANDES (ADMIN)
 *****************************************************************/
