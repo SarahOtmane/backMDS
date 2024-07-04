4f19e6313205b515b180bb086fd6ebfe948bdbd4
SarahOtmane <sarahotmane02@gmail.com>
2024-07-04 19:06:06 +0200

    getAddress

--- src/controllers/adressController.js
+++ src/controllers/adressController.js
@@ -34,3 +34,32 @@ exports.createAnAdress = async (req, res) => {
         res.status(500).json({message: "Erreur lors du traitement des données."});
     }
 };
+
+
+
+
+
+
+/**********************************************************
+            MÉTHODE POUR CREER UNE ADRESSE
+**********************************************************/
+/*
+    Fonction qui permet de créer une adresse
+
+    Les vérifications : 
+        - l existance de l'adresse
+
+*/
+exports.getAddress = async (req, res) => {
+    try {
+        const existingAdress = await Adress.findOne({where: {id: req.params.id_address}});
+        if(!existingAdress){
+            return res.status(401).json({ message: 'Cette adresse n\'existe pas.'});
+        }
+
+        res.status(200).json(existingAdress);
+    } 
+    catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+};
