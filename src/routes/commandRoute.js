9fb83e4b664b76f280097851dc8e2b50db353508
SarahOtmane <sarahotmane02@gmail.com>
2024-07-04 09:52:24 +0200

    config routes pour getCommandOfUser et getAllCommand

---
+++ src/routes/commandRoute.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const commandController = require('../controllers/commandController');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
+
+
+
+router
+    .route('/users')
+    .all(jwtMiddleware.verifyTokenUser)
+    .get(commandController.getCommandOfUser)
+
+
+/**********************************************************
+            ROUTES UNIQUEMENT POUR LES ADMINS
+**********************************************************/  
+router
+    .route('/')
+    .get(commandController.getAllCommand)
+
+
+
+
+
+module.exports = router;
