4334f4cef3e2ec7567e0ad4658cf636e91630328
SarahOtmane <sarahotmane02@gmail.com>
2024-07-04 21:11:48 +0200

    clothController en poo

--- src/controllers/clothConntroller.js
+++ src/controllers/clothConntroller.js
@@ -1,196 +1,200 @@
 const Cloth = require('../models/clothModel');
 const Job = require('../models/jobModel');
 
+class ClothController{
 
-/**********************************************************
-            MÉTHODE POUR CREER UN CLOTH (ADMIN)
-**********************************************************/
-/*
-    Fonction qui permet à l'admin de créer un cloth
+    /**********************************************************
+                MÉTHODE POUR CREER UN CLOTH (ADMIN)
+    **********************************************************/
+    /*
+        Fonction qui permet à l'admin de créer un cloth
 
-    Les vérifications : 
-        - l existance du cloth
+        Les vérifications : 
+            - l existance du cloth
 
-*/
-exports.createACloth = async (req, res) => {
-    try {
-        const job = await Job.findOne({where: {name: req.body.name_job}});
-        if(!job){
-            return res.status(400).json({error: "Job non trouvé"});
+    */
+    static async createACloth (req, res){
+        try {
+            const job = await Job.findOne({where: {name: req.body.name_job}});
+            if(!job){
+                return res.status(400).json({error: "Job non trouvé"});
+            }
+
+            const existingCloth = await Cloth.findOne({ where: { 
+                category: req.body.category,
+                clothType: req.body.clothType,
+                name_job: req.body.name_job
+            } });
+            if (existingCloth) {
+                return res.status(409).json({ message: 'Cet habit existe déjà.' });
+            }
+
+            await Cloth.create({ 
+                category: req.body.category,
+                clothType: req.body.clothType,
+                name_job: req.body.name_job
+            });
+
+            res.status(201).json({ 
+                message: `Habit créé avec succès.` 
+            });
+        } 
+        catch (error) {
+            res.status(500).json({message: "Erreur lors du traitement des données."});
         }
+    };
 
-        const existingCloth = await Cloth.findOne({ where: { 
-            category: req.body.category,
-            clothType: req.body.clothType,
-            name_job: req.body.name_job
-        } });
-        if (existingCloth) {
-            return res.status(409).json({ message: 'Cet habit existe déjà.' });
+
+
+    /**********************************************************
+                MÉTHODE POUR UPDATE UN CLOTH (ADMIN)
+    **********************************************************/
+    /*
+        Fonction qui permet à l'admin de modifier un cloth
+
+        Les vérifications : 
+            - l existance du cloth
+
+    */
+    static async putACloth (req, res){
+        try {
+            const existingCloth = await Cloth.findOne({ where: { id: req.params.id_cloth } });
+            if (!existingCloth) {
+                return res.status(404).json({ message: 'Cet habit non trouvé.' });
+            }
+
+            const job = await Job.findOne({where: {name: req.body.name_job}});
+            if(!job){
+                return res.status(400).json({error: "Job non trouvé"});
+            }
+
+            await existingCloth.update({ 
+                category: req.body.category,
+                clothType: req.body.clothType,
+                name_job: req.body.name_job
+            });
+
+            res.status(200).json({ message: 'Cloth mis à jour avec succès.' });
+        } 
+        catch (error) {
+            res.status(500).json({message: "Erreur lors du traitement des données."});
         }
-
-        await Cloth.create({ 
-            category: req.body.category,
-            clothType: req.body.clothType,
-            name_job: req.body.name_job
-        });
-
-        res.status(201).json({ 
-            message: `Habit créé avec succès.` 
-        });
-    } 
-    catch (error) {
-        res.status(500).json({message: "Erreur lors du traitement des données."});
-    }
-};
+    };
 
 
+    /**********************************************************
+                MÉTHODE POUR SUPPRIMER UN CLOTH (ADMIN)
+    **********************************************************/
+    /*
+        Fonction qui permet de supprimer un cloth
 
-/**********************************************************
-            MÉTHODE POUR UPDATE UN CLOTH (ADMIN)
-**********************************************************/
-/*
-    Fonction qui permet à l'admin de modifier un cloth
+        Les vérifications : 
+            - Vérifier que le cloth existe
 
-    Les vérifications : 
-        - l existance du cloth
+    */
+    static async deleteACloth(req, res){
+        try {
 
-*/
-exports.putACloth = async (req, res) => {
-    try {
-        const existingCloth = await Cloth.findOne({ where: { id: req.params.id_cloth } });
-        if (!existingCloth) {
-            return res.status(404).json({ message: 'Cet habit non trouvé.' });
+            const deleteCloth = await Cloth.destroy({
+                where: { id: req.params.id_cloth }
+            });
+
+            if (!deleteCloth) {
+                return res.status(404).json({ message: 'Habit non trouvé.' });
+            }
+
+            res.status(200).json({ message: 'Habit supprimé avec succès.' });
+
+        } catch (error) {
+            res.status(500).json({message: "Erreur lors du traitement des données."});
         }
+    };
 
-        const job = await Job.findOne({where: {name: req.body.name_job}});
-        if(!job){
-            return res.status(400).json({error: "Job non trouvé"});
+
+    /**********************************************************
+                MÉTHODE POUR LISTER TOUS LES CLOTHES
+    **********************************************************/
+    /*
+        Fonction qui permet de lister tous les clothes
+
+        Les vérifications : 
+            - Vérifier que les clothes existent
+
+    */
+    static async getAllClothes (req, res){
+        try {
+
+            const clothes = await Cloth.findAll();
+
+            if (clothes.length === 0) {
+                return res.status(404).json({ message: 'Auncun cloth trouvé.' });
+            }
+
+            res.status(200).json(clothes);
+
+        } catch (error) {
+            res.status(500).json({message: "Erreur lors du traitement des données."});
         }
-
-        await existingCloth.update({ 
-            category: req.body.category,
-            clothType: req.body.clothType,
-            name_job: req.body.name_job
-        });
-
-        res.status(200).json({ message: 'Cloth mis à jour avec succès.' });
-    } 
-    catch (error) {
-        res.status(500).json({message: "Erreur lors du traitement des données."});
-    }
-};
+    };
 
 
-/**********************************************************
-            MÉTHODE POUR SUPPRIMER UN CLOTH (ADMIN)
-**********************************************************/
-/*
-    Fonction qui permet de supprimer un cloth
 
-    Les vérifications : 
-        - Vérifier que le cloth existe
 
-*/
-exports.deleteACloth = async (req, res) => {
-    try {
+    /**********************************************************
+                MÉTHODE POUR LISTER TOUS LES CLOTHES D'UN JOB
+    **********************************************************/
+    /*
+        Fonction qui permet de lister tous les clothes
 
-        const deleteCloth = await Cloth.destroy({
-            where: { id: req.params.id_cloth }
-        });
-        
-        if (!deleteCloth) {
-            return res.status(404).json({ message: 'Habit non trouvé.' });
+        Les vérifications : 
+            - Vérifier que les clothes existent
+
+    */
+    static async getAllClothesOfJob(req, res){
+        try {
+
+            const job = await Job.findOne({where: {name: req.params.name_job}});
+            if(!job){
+                return res.status(404).json({error: "Job non trouvé"});
+            }
+
+            const clothes = await Cloth.findAll({where: {name_job : req.params.name_job}});
+            if (clothes.length === 0) {
+                return res.status(404).json({ message: 'Auncun cloth trouvé.' });
+            }
+
+            res.status(200).json(clothes);
+
+        } catch (error) {
+            res.status(500).json({message: "Erreur lors du traitement des données."});
         }
-
-        res.status(200).json({ message: 'Habit supprimé avec succès.' });
-
-    } catch (error) {
-        res.status(500).json({message: "Erreur lors du traitement des données."});
-    }
-};
+    };
 
 
-/**********************************************************
-            MÉTHODE POUR LISTER TOUS LES CLOTHES
-**********************************************************/
-/*
-    Fonction qui permet de lister tous les clothes
+    /**********************************************************
+                MÉTHODE POUR RÉCUP LE NAME D'UN CLOTH
+    **********************************************************/
+    /*
+        Fonction qui permet de lister un cloth
 
-    Les vérifications : 
-        - Vérifier que les clothes existent
+        Les vérifications : 
+            - Vérifier que le cloth existe
 
-*/
-exports.getAllClothes = async (req, res) => {
-    try {
-        
-        const clothes = await Cloth.findAll();
-        
-        if (clothes.length === 0) {
-            return res.status(404).json({ message: 'Auncun cloth trouvé.' });
+    */
+    static async getACloth(req, res){
+        try {
+            const cloth = await Cloth.findOne({ where: { id: req.params.id_cloth } });
+
+            if (!cloth) {
+                return res.status(404).json({ message: 'Habit non trouvé.' });
+            }
+
+            res.status(200).json(cloth);
+
+        } catch (error) {
+            res.status(500).json({message: "Erreur lors du traitement des données."});
         }
+    };
+}
 
-        res.status(200).json(clothes);
-
-    } catch (error) {
-        res.status(500).json({message: "Erreur lors du traitement des données."});
-    }
-};
-
-
-
-
-/**********************************************************
-            MÉTHODE POUR LISTER TOUS LES CLOTHES D'UN JOB
-**********************************************************/
-/*
-    Fonction qui permet de lister tous les clothes
-
-    Les vérifications : 
-        - Vérifier que les clothes existent
-
-*/
-exports.getAllClothesOfJob = async (req, res) => {
-    try {
-
-        const job = await Job.findOne({where: {name: req.params.name_job}});
-        if(!job){
-            return res.status(404).json({error: "Job non trouvé"});
-        }
-        
-        const clothes = await Cloth.findAll({where: {name_job : req.params.name_job}});
-        if (clothes.length === 0) {
-            return res.status(404).json({ message: 'Auncun cloth trouvé.' });
-        }
-
-        res.status(200).json(clothes);
-
-    } catch (error) {
-        res.status(500).json({message: "Erreur lors du traitement des données."});
-    }
-};
-
-
-/**********************************************************
-            MÉTHODE POUR RÉCUP LE NAME D'UN CLOTH
-**********************************************************/
-/*
-    Fonction qui permet de lister un cloth
-
-    Les vérifications : 
-        - Vérifier que le cloth existe
-
-*/
-exports.getACloth = async (req, res) => {
-    try {
-        const cloth = await Cloth.findOne({ where: { id: req.params.id_cloth } });
-
-        if (!cloth) {
-            return res.status(404).json({ message: 'Habit non trouvé.' });
-        }
-
-        res.status(200).json(cloth);
-
-    } catch (error) {
-        res.status(500).json({message: "Erreur lors du traitement des données."});
-    }
-};
+module.exports = ClothController;
