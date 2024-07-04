220dd97137fc737937cbc46e7792e135367d4e7f
SarahOtmane <sarahotmane02@gmail.com>
2024-07-04 20:56:29 +0200

    remettre en poo la table adresse

--- src/controllers/adressController.js
+++ src/controllers/adressController.js
@@ -1,65 +1,70 @@
 const Adress = require('../models/adressModel');
 
+class AddressController{
 
-/**********************************************************
-            MÉTHODE POUR CREER UNE ADRESSE
-**********************************************************/
-/*
-    Fonction qui permet de créer une adresse
+    /**********************************************************
+                MÉTHODE POUR CREER UNE ADRESSE
+    **********************************************************/
+    /*
+        Fonction qui permet de créer une adresse
 
-    Les vérifications : 
-        - l existance de l'adresse
+        Les vérifications : 
+            - l existance de l'adresse
 
-*/
-exports.createAnAdress = async (req, res) => {
-    try {
-        const existingAdress = await Adress.findOne({
-            where: {
-                streetAdress : req.body.streetAdress,
-                city : req.body.city,
-                postalCode : req.body.postalCode,
-                country : req.body.country
+    */
+    static async createAnAdress(req, res) {
+        try {
+            const existingAdress = await Adress.findOne({
+                where: {
+                    streetAdress : req.body.streetAdress,
+                    city : req.body.city,
+                    postalCode : req.body.postalCode,
+                    country : req.body.country
+                }
+            });
+
+            if(existingAdress){
+                return res.status(401).json({ message: 'Cette adresse existe déjà.' });
             }
-        });
-
-        if(existingAdress){
-            return res.status(401).json({ message: 'Cette adresse existe déjà.' });
+            await Adress.create(req.body);
+            res.status(201).json({ 
+                message: `Habit créé avec succès.` 
+            });
+        } 
+        catch (error) {
+            res.status(500).json({message: "Erreur lors du traitement des données."});
         }
-        await Adress.create(req.body);
-        res.status(201).json({ 
-            message: `Habit créé avec succès.` 
-        });
-    } 
-    catch (error) {
-        res.status(500).json({message: "Erreur lors du traitement des données."});
-    }
-};
+    };
 
-
-
-
-
-
-/**********************************************************
+    /**********************************************************
             MÉTHODE POUR CREER UNE ADRESSE
-**********************************************************/
-/*
-    Fonction qui permet de créer une adresse
+    **********************************************************/
+    /*
+        Fonction qui permet de créer une adresse
 
-    Les vérifications : 
-        - l existance de l'adresse
+        Les vérifications : 
+            - l existance de l'adresse
 
-*/
-exports.getAddress = async (req, res) => {
-    try {
-        const existingAdress = await Adress.findOne({where: {id: req.params.id_address}});
-        if(!existingAdress){
-            return res.status(401).json({ message: 'Cette adresse n\'existe pas.'});
+    */
+    static async getAddress(req, res){
+        try {
+            const existingAdress = await Adress.findOne({where: {id: req.params.id_address}});
+            if(!existingAdress){
+                return res.status(401).json({ message: 'Cette adresse n\'existe pas.'});
+            }
+
+            res.status(200).json(existingAdress);
+        } 
+        catch (error) {
+            res.status(500).json({message: "Erreur lors du traitement des données."});
         }
+    };
 
-        res.status(200).json(existingAdress);
-    } 
-    catch (error) {
-        res.status(500).json({message: "Erreur lors du traitement des données."});
-    }
-};
+}
+
+
+module.exports = AddressController;
+
+
+
+
--- src/routes/adressRoute.js
+++ src/routes/adressRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const addressController = require('../controllers/adressController');
 const jwtMiddleware = require('../middlewares/jwtMiddleware');
+const AddressController = require('../controllers/adressController');
 
 
 router
     .route('/:id_address')
-    .get(addressController.getAddress)
+    .get(AddressController.getAddress)
 
 module.exports = router;
