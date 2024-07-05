067bae49ab3c5754148f4b637252171223023cbc
SarahOtmane <sarahotmane02@gmail.com>
2024-07-05 14:51:39 +0200

    app en poo

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
-const createServeur = require('./services/serveur');
-const connect = require('./services/connectBdd');
-const createTablesInOrder = require('./services/tablesBdd');
+const Server = require('./services/serveur');
+const Database = require('./services/connectBdd');
+const TablesManager = require('./services/tablesBdd');
 
-const port = 3004;
-
-const app = createServeur();
-
-// Démarrage du serveur
-app.listen(port, async () => {
-    try {
-        await connect();
-
-        createTablesInOrder();
-
-    } catch (error) {
-        console.error('Erreur lors de la connexion à la base de données :', error);
+class App {
+    constructor(port) {
+        this.port = port;
+        this.server = new Server();
     }
-});
+
+    async start() {
+        try {
+            await Database.connect();
+            await TablesManager.createTablesInOrder();
+            this.server.start(this.port);
+        } catch (error) {
+            console.error('Erreur lors de la connexion à la base de données :', error);
+        }
+    }
+}
+
+const app = new App(3004);
+app.start();
