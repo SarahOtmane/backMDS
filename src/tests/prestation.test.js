ec091440482218514929f10aba3009ab27b084ca
SarahOtmane <sarahotmane02@gmail.com>
2024-07-14 15:35:25 +0200

    getAPrestation test

--- src/tests/prestation.test.js
+++ src/tests/prestation.test.js
@@ -35,5 +35,24 @@ describe('Prestation controller', () => {
             expect(statusCode).toBe(404);
         });
     });
+
+    describe('GET /prestations/:id_prestation', () => {
+        it('should return 201 when getting a presta by its id', async() => {
+            await Job.create({ name: 'Test' });
+            const presta = await Prestation.create(dataPrestation);
+
+            const { statusCode } = await supertest(app)
+            .get(`/prestations/${presta.id}`)
+
+            expect(statusCode).toBe(201);
+        });
+        
+        it('should return 404 when no prestation is found', async() => {
+            const { statusCode } = await supertest(app)
+                .get('/prestations/9999');
+
+            expect(statusCode).toBe(404);
+        })
+    });
     
 });
