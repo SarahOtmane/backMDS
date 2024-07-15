e15e19eff3d3b5f0baf8f9a91f6bddb8a25f94df
SarahOtmane <sarahotmane02@gmail.com>
2024-07-15 21:15:16 +0200

    update de tous les fichiers tests

--- src/tests/cloth.test.js
+++ src/tests/cloth.test.js
@@ -1,204 +1,204 @@
-// const Server = require('../services/serveur');
-// const Job = require('../models/jobModel');
-// const Cloth = require('../models/clothModel');
-// const supertest = require('supertest');
+const Server = require('../services/serveur');
+const Job = require('../models/jobModel');
+const Cloth = require('../models/clothModel');
+const supertest = require('supertest');
 
-// const app = new Server().app;
+const app = new Server().app;
 
-// let token;
+let token;
 
-// describe('Cloth controller', () => {
-//     beforeAll(async() =>{
-//         const response = await supertest(app)
-//             .post(`/persons/login`)
-//             .send({
-//                 email: 'sarahotmane02@gmail.com',
-//                 password: 'S@rah2024'
-//             });
+describe('Cloth controller', () => {
+    beforeAll(async() =>{
+        const response = await supertest(app)
+            .post(`/persons/login`)
+            .send({
+                email: 'sarahotmane02@gmail.com',
+                password: 'S@rah2024'
+            });
         
-//             token = response.body.token;
-//     });
+            token = response.body.token;
+    });
 
-//     afterEach(async () => {
-//         await Cloth.destroy({where: {name_job: 'testt'}});
-//         await Job.destroy({where: {name: 'testt'}});
-//     });
+    afterEach(async () => {
+        await Cloth.destroy({where: {name_job: 'testt'}});
+        await Job.destroy({where: {name: 'testt'}});
+    });
 
-//     describe('POST /clothes', () => {
-//         it('should return 201 when creating a new cloth', async () => {
-//             await Job.create({name: 'testt'});
+    describe('POST /clothes', () => {
+        it('should return 201 when creating a new cloth', async () => {
+            await Job.create({name: 'testt'});
 
-//             const { statusCode } = await supertest(app)
-//                 .post('/clothes')
-//                 .send({
-//                     category: 'Test1',
-//                     clothType: 'T-shirt',
-//                     name_job: 'testt',
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+            const { statusCode } = await supertest(app)
+                .post('/clothes')
+                .send({
+                    category: 'Test1',
+                    clothType: 'T-shirt',
+                    name_job: 'testt',
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(201);
-//         });
+            expect(statusCode).toBe(201);
+        });
 
-//         it('should return 409 when a cloth already exists', async () => {
-//             await Job.create({name: 'testt'});
-//             await Cloth.create({category: 'Test1', clothType: 'T-shirt', name_job: 'testt'});
+        it('should return 409 when a cloth already exists', async () => {
+            await Job.create({name: 'testt'});
+            await Cloth.create({category: 'Test1', clothType: 'T-shirt', name_job: 'testt'});
 
-//             const { statusCode } = await supertest(app)
-//                 .post('/clothes')
-//                 .send({
-//                     category: 'Test1',
-//                     clothType: 'T-shirt',
-//                     name_job: 'testt',
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+            const { statusCode } = await supertest(app)
+                .post('/clothes')
+                .send({
+                    category: 'Test1',
+                    clothType: 'T-shirt',
+                    name_job: 'testt',
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(409);
-//         });
+            expect(statusCode).toBe(409);
+        });
 
-//         it('should return 400 when the job is not found', async () => {
-//             const { statusCode } = await supertest(app)
-//                 .post('/clothes')
-//                 .send({
-//                     category: 'Haut',
-//                     clothType: 'T-shirt',
-//                     name_job: 'nonexistent',
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+        it('should return 400 when the job is not found', async () => {
+            const { statusCode } = await supertest(app)
+                .post('/clothes')
+                .send({
+                    category: 'Haut',
+                    clothType: 'T-shirt',
+                    name_job: 'nonexistent',
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(400);
-//         });
-//     });
+            expect(statusCode).toBe(400);
+        });
+    });
 
-//     describe('PUT /clothes/:id_cloth', () => {
-//         it('should return 200 when updating a cloth', async () => {
-//             await Job.create({name: 'testt'});
-//             const cloth = await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
+    describe('PUT /clothes/:id_cloth', () => {
+        it('should return 200 when updating a cloth', async () => {
+            await Job.create({name: 'testt'});
+            const cloth = await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
 
-//             const { statusCode } = await supertest(app)
-//                 .put(`/clothes/${cloth.id}`)
-//                 .send({
-//                     category: 'Haut',
-//                     clothType: 'Doudoune',
-//                     name_job: 'testt',
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+            const { statusCode } = await supertest(app)
+                .put(`/clothes/${cloth.id}`)
+                .send({
+                    category: 'Haut',
+                    clothType: 'Doudoune',
+                    name_job: 'testt',
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(200);
-//         });
+            expect(statusCode).toBe(200);
+        });
 
-//         it('should return 404 when the cloth is not found', async () => {
-//             const { statusCode } = await supertest(app)
-//                 .put('/clothes/9999')
-//                 .send({
-//                     category: 'Haut',
-//                     clothType: 'Vestes',
-//                     name_job: 'testt',
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+        it('should return 404 when the cloth is not found', async () => {
+            const { statusCode } = await supertest(app)
+                .put('/clothes/9999')
+                .send({
+                    category: 'Haut',
+                    clothType: 'Vestes',
+                    name_job: 'testt',
+                })
+                .set('Authorization', `Bearer ${token}`);
                 
-//             expect(statusCode).toBe(404);
-//         });
+            expect(statusCode).toBe(404);
+        });
 
-//         it('should return 400 when the job is not found', async () => {
-//             await Job.create({name: 'testt'});
-//             const cloth = await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
+        it('should return 400 when the job is not found', async () => {
+            await Job.create({name: 'testt'});
+            const cloth = await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
     
-//             const { statusCode } = await supertest(app)
-//                 .put(`/clothes/${cloth.id}`)
-//                 .send({
-//                     category: 'Haut',
-//                     clothType: 'T-shirt',
-//                     name_job: 'nonexistent',
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+            const { statusCode } = await supertest(app)
+                .put(`/clothes/${cloth.id}`)
+                .send({
+                    category: 'Haut',
+                    clothType: 'T-shirt',
+                    name_job: 'nonexistent',
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(400);
-//         });
-//     });
+            expect(statusCode).toBe(400);
+        });
+    });
 
-//     describe('GET /clothes', () => {
-//         it('should return 200 when getting all clothes', async () => {
-//             await Job.create({name: 'testt'});
-//             await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
+    describe('GET /clothes', () => {
+        it('should return 200 when getting all clothes', async () => {
+            await Job.create({name: 'testt'});
+            await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
 
-//             const { statusCode, body } = await supertest(app)
-//                 .get('/clothes');
+            const { statusCode, body } = await supertest(app)
+                .get('/clothes');
 
-//             expect(statusCode).toBe(200);
-//         });
+            expect(statusCode).toBe(200);
+        });
 
-//         it('should return 404 when no cloth is found', async () => {
-//             await Cloth.destroy({where: {}});
-//             const { statusCode, body } = await supertest(app)
-//                 .get('/clothes');
+        it('should return 404 when no cloth is found', async () => {
+            await Cloth.destroy({where: {}});
+            const { statusCode, body } = await supertest(app)
+                .get('/clothes');
 
-//             expect(statusCode).toBe(404);
-//         });
-//     });
+            expect(statusCode).toBe(404);
+        });
+    });
 
-//     describe('GET /clothes/job/:name_job', () => {
-//         it('should return 200 when getting all clothes of a job', async () => {
-//             await Job.create({name: 'testt'});
-//             await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
+    describe('GET /clothes/job/:name_job', () => {
+        it('should return 200 when getting all clothes of a job', async () => {
+            await Job.create({name: 'testt'});
+            await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
 
-//             const { statusCode, body } = await supertest(app)
-//                 .get('/clothes/job/testt');
+            const { statusCode, body } = await supertest(app)
+                .get('/clothes/job/testt');
 
-//             expect(statusCode).toBe(200);
-//         });
+            expect(statusCode).toBe(200);
+        });
 
-//         it('should return 404 when no cloth is found', async () => {
-//             const { statusCode, body } = await supertest(app)
-//                 .get('/clothes/job/test');
+        it('should return 404 when no cloth is found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .get('/clothes/job/test');
 
-//             expect(statusCode).toBe(404);
-//         });
+            expect(statusCode).toBe(404);
+        });
 
-//         it('should return 404 when the job is not found', async () => {
-//             const { statusCode, body } = await supertest(app)
-//                 .get('/clothes/job/teeeeest');
+        it('should return 404 when the job is not found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .get('/clothes/job/teeeeest');
 
-//             expect(statusCode).toBe(404);
-//         });
-//     });
+            expect(statusCode).toBe(404);
+        });
+    });
 
-//     describe('GET /clothes/:id_cloth', () => {
-//         it('should return 200 when getting the details of a cloth', async () => {
-//             await Job.create({name: 'testt'});
-//             const cloth = await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
+    describe('GET /clothes/:id_cloth', () => {
+        it('should return 200 when getting the details of a cloth', async () => {
+            await Job.create({name: 'testt'});
+            const cloth = await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
 
-//             const { statusCode } = await supertest(app)
-//                 .get(`/clothes/${cloth.id}`)
-//             expect(statusCode).toBe(200);
-//         });
+            const { statusCode } = await supertest(app)
+                .get(`/clothes/${cloth.id}`)
+            expect(statusCode).toBe(200);
+        });
 
-//         it('should return 404 when the cloth is not found', async () => {
-//             const { statusCode } = await supertest(app)
-//                 .get('/clothes/9999')
-//             expect(statusCode).toBe(404);
-//         });
-//     });
+        it('should return 404 when the cloth is not found', async () => {
+            const { statusCode } = await supertest(app)
+                .get('/clothes/9999')
+            expect(statusCode).toBe(404);
+        });
+    });
 
-//     describe('DELETE /clothes/:id_cloth', () => {
-//         it('should return 200 when deleting the cloth', async () => {
-//             await Job.create({name: 'testt'});
-//             const cloth = await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
+    describe('DELETE /clothes/:id_cloth', () => {
+        it('should return 200 when deleting the cloth', async () => {
+            await Job.create({name: 'testt'});
+            const cloth = await Cloth.create({category: 'Haut', clothType: 'T-shirt', name_job: 'testt'});
 
-//             const { statusCode } = await supertest(app)
-//                 .delete(`/clothes/${cloth.id}`)
-//                 .set('Authorization', `Bearer ${token}`);
+            const { statusCode } = await supertest(app)
+                .delete(`/clothes/${cloth.id}`)
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(200);
-//         });
+            expect(statusCode).toBe(200);
+        });
 
-//         it('should return 404 when the cloth is not found', async () => {
-//             const { statusCode, body } = await supertest(app)
-//                 .delete('/clothes/9999')
-//                 .set('Authorization', `Bearer ${token}`);
+        it('should return 404 when the cloth is not found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .delete('/clothes/9999')
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(404);
-//         });
-//     });
+            expect(statusCode).toBe(404);
+        });
+    });
 
-// });
+});
--- src/tests/info.test.js
+++ src/tests/info.test.js
@@ -1,136 +1,136 @@
-// const Server = require('../services/serveur');
-// const Info = require('../models/infoModel');
-// const supertest = require('supertest');
+const Server = require('../services/serveur');
+const Info = require('../models/infoModel');
+const supertest = require('supertest');
 
-// const app = new Server().app;
+const app = new Server().app;
 
 
-// describe('Info controller', () => {
-//     beforeAll(async() =>{
-//         const response = await supertest(app)
-//             .post(`/persons/login`)
-//             .send({
-//                 email: 'sarahotmane02@gmail.com',
-//                 password: 'S@rah2024'
-//             });
+describe('Info controller', () => {
+    beforeAll(async() =>{
+        const response = await supertest(app)
+            .post(`/persons/login`)
+            .send({
+                email: 'sarahotmane02@gmail.com',
+                password: 'S@rah2024'
+            });
         
-//             token = response.body.token;
-//     });
+            token = response.body.token;
+    });
 
-//     afterEach(async() =>{
-//         await Info.destroy({where: {name: 'Test1'}});
-//     })
+    afterEach(async() =>{
+        await Info.destroy({where: {name: 'Test1'}});
+    })
 
-//     describe('POST /infos', () => {
-//         it('should return 201 when creating a new info', async () => {
-//             const { statusCode, body } = await supertest(app)
-//                 .post('/infos')
-//                 .send({
-//                     name: 'Test1',
-//                     content: 'Content1'
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+    describe('POST /infos', () => {
+        it('should return 201 when creating a new info', async () => {
+            const { statusCode, body } = await supertest(app)
+                .post('/infos')
+                .send({
+                    name: 'Test1',
+                    content: 'Content1'
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(201);
-//         });
+            expect(statusCode).toBe(201);
+        });
 
-//         it('should return 401 when an info already exist', async () => {
-//             await Info.create({name: 'Test1', content: 'Content1' });
+        it('should return 401 when an info already exist', async () => {
+            await Info.create({name: 'Test1', content: 'Content1' });
 
-//             const { statusCode, body } = await supertest(app)
-//                 .post('/infos')
-//                 .send({
-//                     name: 'Test1',
-//                     content: 'Content1'
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+            const { statusCode, body } = await supertest(app)
+                .post('/infos')
+                .send({
+                    name: 'Test1',
+                    content: 'Content1'
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(401);
-//         });
-//     });
+            expect(statusCode).toBe(401);
+        });
+    });
 
 
-//     describe('GET /infos', () => {
-//         it('should return 201 when getting all infos', async () => {
-//             await Info.create({name: 'Test1', content: 'Content1' });
-//             const { statusCode, body } = await supertest(app)
-//                 .get('/infos')
+    describe('GET /infos', () => {
+        it('should return 201 when getting all infos', async () => {
+            await Info.create({name: 'Test1', content: 'Content1' });
+            const { statusCode, body } = await supertest(app)
+                .get('/infos')
 
-//             expect(statusCode).toBe(201);
-//         });
+            expect(statusCode).toBe(201);
+        });
 
-//         it('should return 404 when no info is found', async () => {
-//             const { statusCode, body } = await supertest(app)
-//                 .get('/infos')
+        it('should return 404 when no info is found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .get('/infos')
 
-//             expect(statusCode).toBe(404);
-//         });
-//     });
+            expect(statusCode).toBe(404);
+        });
+    });
 
-//     describe('GET /infos/:name_info', () => {
-//         it('should return 201 when getting the details of info', async () => {
-//             const info = await Info.create({name: 'Test1', content: 'Content1' });
+    describe('GET /infos/:name_info', () => {
+        it('should return 201 when getting the details of info', async () => {
+            const info = await Info.create({name: 'Test1', content: 'Content1' });
 
-//             const { statusCode, body } = await supertest(app)
-//                 .get(`/infos/${info.name}`);
+            const { statusCode, body } = await supertest(app)
+                .get(`/infos/${info.name}`);
 
-//             expect(statusCode).toBe(201);
-//         });
+            expect(statusCode).toBe(201);
+        });
 
-//         it('should return 404 when the info is not found', async () => {
-//             const { statusCode, body } = await supertest(app)
-//                 .get(`/infos/test`);
+        it('should return 404 when the info is not found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .get(`/infos/test`);
 
-//             expect(statusCode).toBe(404);
-//         });
-//     });
+            expect(statusCode).toBe(404);
+        });
+    });
 
-//     describe('PUT /infos/:name_info', () => {
-//         it('should return 201 when updating the info', async () => {
-//             const info = await Info.create({name: 'Test1', content: 'Content1' });
+    describe('PUT /infos/:name_info', () => {
+        it('should return 201 when updating the info', async () => {
+            const info = await Info.create({name: 'Test1', content: 'Content1' });
 
-//             const { statusCode, body } = await supertest(app)
-//                 .put(`/infos/${info.name}`)
-//                 .send({
-//                     name: 'Test1', 
-//                     content: 'Content3'
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+            const { statusCode, body } = await supertest(app)
+                .put(`/infos/${info.name}`)
+                .send({
+                    name: 'Test1', 
+                    content: 'Content3'
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(201);
-//         });
+            expect(statusCode).toBe(201);
+        });
 
-//         it('should return 404 when the info is not found', async () => {
-//             const { statusCode, body } = await supertest(app)
-//                 .put(`/infos/teeest`)
-//                 .send({
-//                     name: 'Test1', 
-//                     content: 'Content3'
-//                 })
-//                 .set('Authorization', `Bearer ${token}`);
+        it('should return 404 when the info is not found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .put(`/infos/teeest`)
+                .send({
+                    name: 'Test1', 
+                    content: 'Content3'
+                })
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(404);
-//         });
-//     });
+            expect(statusCode).toBe(404);
+        });
+    });
 
-//     describe('DELETE /infos/:name_info', () => {
-//         it('should return 201 when deleting the info', async () => {
-//             const info = await Info.create({name: 'Test1', content: 'Content1' });
+    describe('DELETE /infos/:name_info', () => {
+        it('should return 201 when deleting the info', async () => {
+            const info = await Info.create({name: 'Test1', content: 'Content1' });
 
-//             const { statusCode, body } = await supertest(app)
-//                 .delete(`/infos/${info.name}`)
-//                 .set('Authorization', `Bearer ${token}`);
+            const { statusCode, body } = await supertest(app)
+                .delete(`/infos/${info.name}`)
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(201);
-//         });
+            expect(statusCode).toBe(201);
+        });
 
-//         it('should return 404 when the info is found', async () => {
-//             const { statusCode, body } = await supertest(app)
-//                 .delete('/info/teeeeest')
-//                 .set('Authorization', `Bearer ${token}`);
+        it('should return 404 when the info is found', async () => {
+            const { statusCode, body } = await supertest(app)
+                .delete('/info/teeeeest')
+                .set('Authorization', `Bearer ${token}`);
 
-//             expect(statusCode).toBe(404);
-//         });
-//     });
+            expect(statusCode).toBe(404);
+        });
+    });
 
-// });
+});
