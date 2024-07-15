032adab34c46bfe2d744eb31d1cb24bdd13d06bb
SarahOtmane <sarahotmane02@gmail.com>
2024-07-15 13:33:53 +0200

    get and post tests

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -22,12 +22,26 @@ let dataPrestation = {
     name_job: 'Test'
 };
 
+let token;
+
 describe('Product controller', () => {
     afterEach( async() => {
+        await Product.destroy({where: {price: 20}});
         await Artisan.destroy({ where: { siret: '123456789' } });
         await Prestation.destroy({ where: { name_job: 'Test' } });
         await Job.destroy({ where: { name: 'Test' } });
     });
+
+    beforeAll(async() =>{
+        const response = await supertest(app)
+            .post(`/persons/login`)
+            .send({
+                email: 'otmanesarah02@gmail.com',
+                password: 'SARAHtest@2024'
+            });
+
+        token = response.body.token;
+    })
 
     describe('GET /products/:id_artisan/:id_prestation', () => {
         it('should return 404 if no artisan is found', async() => {
@@ -72,6 +86,159 @@ describe('Product controller', () => {
                 .get(`/products/${artisan.id}/${presta.id}`);
 
             expect(statusCode).toBe(201);
+        });
+    });
+    
+
+    describe('GET /products/:id_product', () => {
+        it('should return 404 if no product is found', async() => {
+            const { statusCode, body } = await supertest(app)
+                .get(`/products/9999`);
+
+            expect(statusCode).toBe(404);
+        });
+
+        it('should return 404 when no artisan is found', async() => {
+            const product = await Product.create({
+                id_artisan: null,
+                id_prestation: null,
+                price: 20
+            });
+
+            const { statusCode, body } = await supertest(app)
+                .get(`/products/${product.id}`);
+
+            expect(statusCode).toBe(404);
+        });
+
+        it('should return 404 when no presta is found', async() => {
+            await Job.create({name: 'Test'});
+            const artisan = await Artisan.create(dataArtisan);
+            const product = await Product.create({
+                id_artisan: artisan.id,
+                id_prestation: null,
+                price: 20
+            });
+
+            const { statusCode, body } = await supertest(app)
+                .get(`/products/${product.id}`);
+
+            expect(statusCode).toBe(404);
+        });
+
+        it('should return 201 when getting a product', async() => {
+            await Job.create({name: 'Test'});
+            const artisan = await Artisan.create(dataArtisan);
+            const presta = await Prestation.create(dataPrestation);
+            const product = await Product.create({
+                id_artisan: artisan.id,
+                id_prestation: presta.id,
+                price: 20
+            });
+
+            const { statusCode, body } = await supertest(app)
+                .get(`/products/${product.id}`);
+
+            expect(statusCode).toBe(201);
+        });
+        
+    });
+
+    describe('GET /products/artisan/:id_artisan', () => {
+        it('should return 404 if no artisan is found', async() => {
+            const { statusCode, body } = await supertest(app)
+                .get(`/products/artisan/9999`)
+                .set('Authorization', 'Bearer ' + token);
+
+            expect(statusCode).toBe(404);
+        });
+
+        it('should return 404 if no artisan is found', async() => {
+            await Job.create({name: 'Test'});
+            const artisan = await Artisan.create(dataArtisan);
+
+            const { statusCode, body } = await supertest(app)
+                .get(`/products/artisan/${artisan.id}`)
+                .set('Authorization', 'Bearer ' + token);
+
+            expect(statusCode).toBe(404);
+        });
+
+        it('should return 201 when getting all products of an artisan', async() => {
+            await Job.create({name: 'Test'});
+            const artisan = await Artisan.create(dataArtisan);
+            const presta = await Prestation.create(dataPrestation);
+            await Product.create({
+                id_artisan: artisan.id,
+                id_prestation: presta.id,
+                price: 20
+            });
+
+            const { statusCode, body } = await supertest(app)
+                .get(`/products/artisan/${artisan.id}`)
+                .set('Authorization', 'Bearer ' + token);
+
+            expect(statusCode).toBe(201);
+        });
+    });
+    
+
+    describe('POST /products/artisan/:id_artisan', () => {
+        it('should return 404 if the artisan is found', async() => {
+            const { statusCode } = await supertest(app)
+                .post(`/products/artisan/9999`)
+                .send({
+                    reparationType : 'testNettoyer',
+                    price : 20
+                })
+                .set('Authorization', 'Bearer ' + token);
+
+            expect(statusCode).toBe(404);
+        });
+
+        it('should return 404 if the prestation is not found', async() => {
+            await Job.create({name: 'Test'});
+            const artisan = await Artisan.create(dataArtisan);
+
+            const { statusCode } = await supertest(app)
+                .post(`/products/artisan/${artisan.id}`)
+                .send({
+                    reparationType : 'testNettoyer',
+                    price : 20
+                })
+                .set('Authorization', 'Bearer ' + token);
+
+            expect(statusCode).toBe(404);
+        });
+
+        it('should return 201 when creating a product', async() => {
+            await Job.create({name: 'Test'});
+            const artisan = await Artisan.create(dataArtisan);
+            await Prestation.create(dataPrestation);
+
+            const { statusCode } = await supertest(app)
+                .post(`/products/artisan/${artisan.id}`)
+                .send({
+                    reparationType : 'testNettoyer',
+                    price : 20
+                })
+                .set('Authorization', 'Bearer ' + token);
+
+            expect(statusCode).toBe(201);
+        });
+    });
+    
+    describe('PUT /products/:id_product', () => {
+        it('should return 404 if the artisan is found', async() => {
+            const { statusCode } = await supertest(app)
+                .post(`/products/9999`)
+                .send({
+                    reparationType : 'testNettoyer',
+                    price : 20
+                })
+                .set('Authorization', 'Bearer ' + token);
+
+            expect(statusCode).toBe(404);
         });
     });
     
